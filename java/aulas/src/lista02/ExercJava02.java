b0540cf04aa35304f09200bc5d47104a90a2f810
Nicoli <nicoli1992@gmail.com>
2021-02-26 09:02:01 -0300

    atualizando exercício 2 e 4 de java da lista 2

--- java/aulas/src/lista02/ExercJava02.java
+++ java/aulas/src/lista02/ExercJava02.java
@@ -14,6 +14,25 @@ public class ExercJava02 {
 		num1 = ler.nextInt();
 		num2 = ler.nextInt();
 		num3 = ler.nextInt();
+				
+		if ((num1 <= num2) && (num2 <= num3)) {
+			System.out.printf("%d %d %d", num1, num2, num3);
+		} 
+		else if ((num1 <= num3) && (num3 <= num2)) {
+			System.out.printf("%d %d %d", num1, num3, num2);
+		}
+		else if ((num2 <= num1) && (num1 <= num3)) {
+			System.out.printf("%d %d %d", num2, num1, num3);
+		}
+		else if ((num2 <= num3) && (num3 <= num1)) {
+			System.out.printf("%d %d %d", num2, num3, num1);
+		}
+		else if ((num3 <= num1) && (num1 <= num2)) {
+			System.out.printf("%d %d %d", num3, num1, num2);
+		}
+		else if ((num3 <= num2) && (num2 <= num1)) {
+			System.out.printf("%d %d %d", num3, num2, num1);
+		}
 		
 		//System.out.printf("%d %d %d", num1, num2, num3);
 		
--- java/aulas/src/lista02/ExercJava04.java
+++ java/aulas/src/lista02/ExercJava04.java
@@ -8,19 +8,17 @@ public class ExercJava04 {
 
 		Scanner ler = new Scanner(System.in);
 
-		int numero, potenciaImpar;
-		double raizPar;
-		
+		int numero;
 		System.out.print("Informe um número: ");
 		numero = ler.nextInt();
-		raizPar = Math.sqrt(numero);
-		potenciaImpar = (int)Math.pow(numero, 2);
 		
 		if (numero % 2 == 0) {
-			System.out.printf("O número %d é par e sua raíz quadrada é %.2f.", numero, raizPar);
+			System.out.printf("O número %d é par e sua raíz quadrada é %.2f.", 
+					numero, Math.sqrt(numero));
 		}
 		else {
-			System.out.printf("O número %d é ímpar e sua potência ao quadrado é %d.", numero, potenciaImpar);
+			System.out.printf("O número %d é ímpar e sua potência ao quadrado é %d.", 
+					numero, (int)Math.pow(numero, 2));
 		}
 
 		ler.close();
